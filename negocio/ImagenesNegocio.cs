426a34efc42828837cf1354f1bebc9e1a4cc2d07
fpablogaston <fpablogaston12@gmail.com>
2025-10-01 22:35:13 -0300

    Agregue las clases clientes, imagenes y vouchers. Ademas cree las clases de negocio tambien

---
+++ negocio/ImagenesNegocio.cs
@@ -0,0 +1,41 @@
+﻿using dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace negocio
+{
+    public class ImagenesNegocio
+    {
+        public List<Imagenes> listar()
+        {
+            List<Imagenes> lista = new List<Imagenes>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setQuery("SELECT Id, IdArticulo, ImagenUrl FROM Imagenes");
+                datos.ejecutarLectura();
+                while (datos.Lector.Read())
+                {
+                    Imagenes aux = new Imagenes();
+                    aux.Id = (int)datos.Lector["Id"];
+                    aux.IdArticulo = (int)datos.Lector["IdArticulo"];
+                    aux.ImagenUrl = (string)datos.Lector["ImagenUrl"];
+                    lista.Add(aux);
+                }
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+    }
+}
---
+++ negocio/VoucherNegocio.cs
@@ -0,0 +1,42 @@
+﻿using dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace negocio
+{
+    public class VoucherNegocio
+    {
+        public List<Voucher> listar()
+        {
+            List<Voucher> lista = new List<Voucher>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setQuery("SELECT IdCliente, CodigoVoucher, FechaCanje, IdArticulo FROM Vouchers");
+                datos.ejecutarLectura();
+                while (datos.Lector.Read())
+                {
+                    Voucher aux = new Voucher();
+                    aux.CodigoVoucher = (string)datos.Lector["CodigoVoucher"];
+                    aux.IdCliente = (int)datos.Lector["IdCliente"];
+                    aux.FechaCanje = (DateTime)datos.Lector["FechaCanje"];
+                    aux.IdArticulo = (int)datos.Lector["IdArticulo"];
+                    lista.Add(aux);
+                }
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+    }
+}
